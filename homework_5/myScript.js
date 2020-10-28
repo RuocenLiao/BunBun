941dac2d15e8285298f88dfed920f1a49785a28d
RuocenLiao <ruocenl@andrew.cmu.edu>
2020-10-29 01:23:02 +0800

    hw6_v3

---
+++ homework_5/myScript.js
@@ -0,0 +1,68 @@
+function select1() {
+    document.getElementById('1').style.color='black';
+    document.getElementById('1').style.backgroundColor='#FED8B1';
+    document.getElementById('1').style.boxShadow='1px 1px rgb(100, 0, 0, 0.73)';
+
+    document.getElementById('3').style.color='rgb(100, 0, 0, 0.80)';
+    document.getElementById('3').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('3').style.boxShadow='0px 0px'; 
+
+    document.getElementById('6').style.color='rgb(100, 0, 0, 0.80)';
+    document.getElementById('6').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('6').style.boxShadow='0px 0px'; 
+
+    document.getElementById('12').style.color='rgb(100, 0, 0, 0.80)';
+    document.getElementById('12').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('12').style.boxShadow='0px 0px'; 
+};
+function select3() {
+    document.getElementById('1').style.color='rgb(100, 0, 0, 0.73)';
+    document.getElementById('1').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('1').style.boxShadow='0px 0px'; 
+
+    document.getElementById('3').style.color='black';
+    document.getElementById('3').style.backgroundColor='#FED8B1';
+    document.getElementById('3').style.boxShadow='1px 1px rgb(100, 0, 0, 0.73)'; 
+
+    document.getElementById('6').style.color='grgb(100, 0, 0, 0.73)';
+    document.getElementById('6').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('6').style.boxShadow='0px 0px'; 
+
+    document.getElementById('12').style.color='rgb(100, 0, 0, 0.73)';
+    document.getElementById('12').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('12').style.boxShadow='0px 0px'; 
+};
+function select6() {
+    document.getElementById('1').style.color='rgb(100, 0, 0, 0.73)';
+    document.getElementById('1').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('1').style.boxShadow='0px 0px'; 
+
+    document.getElementById('3').style.color='rgb(100, 0, 0, 0.73)';
+    document.getElementById('3').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('3').style.boxShadow='0px 0px'; 
+
+    document.getElementById('6').style.color='black';
+    document.getElementById('6').style.backgroundColor='#FED8B1';
+    document.getElementById('6').style.boxShadow='1px 1px rgb(100, 0, 0, 0.73)'; 
+
+    document.getElementById('12').style.color='rgb(100, 0, 0, 0.73)';
+    document.getElementById('12').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('12').style.boxShadow='0px 0px'; 
+};
+function select12() {
+    document.getElementById('1').style.color='rgb(100, 0, 0, 0.73)';
+    document.getElementById('1').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('1').style.boxShadow='0px 0px'; 
+
+    document.getElementById('3').style.color='rgb(100, 0, 0, 0.73)';
+    document.getElementById('3').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('3').style.boxShadow='0px 0px'; 
+
+    document.getElementById('6').style.color='rgb(100, 0, 0, 0.73)';
+    document.getElementById('6').style.backgroundColor='rgb(254,216,177, 0.3)';
+    document.getElementById('6').style.boxShadow='0px 0px'; 
+
+    document.getElementById('12').style.color='black';
+    document.getElementById('12').style.backgroundColor='#FED8B1';
+    document.getElementById('12').style.boxShadow='1px 1px rgb(100, 0, 0, 0.73)'; 
+};
