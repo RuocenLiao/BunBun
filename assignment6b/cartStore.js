68f0633019014fb0d7b49b6e5194b25d9f3530a4
RuocenLiao <54823131+RuocenLiao@users.noreply.github.com>
2020-11-08 20:03:32 +0800

    test

--- assignment6b/cartStore.js
+++ assignment6b/cartStore.js
@@ -9,6 +9,8 @@
 */
 //localStorage.clear();
 
+
+//currently adding extra br when rewriting
 const recoveredList = JSON.parse(localStorage.getItem('list') || '[]');
 
 if (document.readyState == 'loading') {
@@ -39,7 +41,6 @@ function ready() {
                 const price = recoveredList[i].price;
                 const imgsrc = recoveredList[i].imgsrc;
                 addItemToCart(flavor, quantity, topping, price, imgsrc);
-                console.log("add cart item")
               }
         }
         
@@ -81,15 +82,10 @@ function addToCartClicked(event) {
     var imgsrc = shopItem.getElementsByClassName('detailsImage2')[0].src
     let item = new cartItem(flavor, quantity, topping, price, imgsrc);
 
-    console.log(flavor);
-    console.log(quantity);
-    console.log(price);
-
     //stores item info in the list
     //and loops through to create cart list when switch to cart page
     recoveredList.push(item)
-    console.log(recoveredList.valueOf())
-    console.log(recoveredList[0].flavor)
+    //console.log(recoveredList.valueOf())
     
     localStorage.setItem('list', JSON.stringify(recoveredList));
 }
@@ -105,7 +101,8 @@ function addItemToCart(flavor, quantity, topping, price, imgsrc){
         </span>
     
         <span class="left itemInfo">
-            <span class="cartText left flavor">${flavor}<br></span>
+            <span class="cartText left flavor">${flavor}</span>
+            <br>
             <span class="cartText left cartPrice">${price}</span>
             <br><br>
             <span class="cartText button white smallLable alignEnd quantity">${quantity}</span> 
@@ -130,17 +127,19 @@ function updateCartTotal() {
     var list = []
     for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cartPrice')[0]
+        var priceElement = cartRow.getElementsByClassName('cartPrice')[0].innerHTML
         var flavor = cartRow.getElementsByClassName('flavor')[0].innerHTML
         var quantity = cartRow.getElementsByClassName('quantity')[0].innerHTML
         var topping = cartRow.getElementsByClassName('topping')[0].innerHTML
         var imgsrc = cartRow.getElementsByClassName('cartImage')[0].src
 
+        let item = new cartItem(flavor, quantity, topping, priceElement, imgsrc)
+        list.push(item)
+
         var price = parseFloat(priceElement.innerText.replace('$', ''))
         total = total + price
 
-        let item = new cartItem(flavor, quantity, topping, price, imgsrc)
-        list.push(item)
+        //cartRow.getElementsByClassName('cartPrice')[0] = '$' + priceElement
     }
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('totalPrice')[0].innerText = '$' + total
@@ -156,14 +155,11 @@ function updateCounts(count) {
     let c2 = document.getElementById("count");
     if (c1) {
         c1.innerHTML=cs;
-        console.log("c1");
     }
     if (c2) {
         c2.innerHTML=cs;
-        console.log("c2");
     }
 
     s.count = count;
     localStorage.setItem("store", JSON.stringify(s));
-    console.log(s.count);
 }
